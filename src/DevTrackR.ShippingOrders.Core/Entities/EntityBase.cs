6ff2def7cc62e21631a4c9edc86cca96fdf99da5
Matheus <matheuscardosoc@outlook.com>
2024-06-16 12:58:28 -0300

    adicionado pacote Microsoft.Extensions.DependencyInjection.Abstractions e feita a base do Core para ínicio da implementação

---
+++ src/DevTrackR.ShippingOrders.Core/Entities/EntityBase.cs
@@ -0,0 +1,12 @@
+namespace DevTrackR.ShippingOrders.Core.Entities
+{
+    public abstract class EntityBase
+    {
+        public EntityBase()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
